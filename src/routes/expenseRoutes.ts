ede92d85f3d2a495a092d56036ce1e2794faf1a5
Vidya Vipul <i.vidyavipul@gmail.com>
2026-02-01 15:13:05 +0530

    feat: add missing expense and summary routes

--- src/routes/expenseRoutes.ts
+++ src/routes/expenseRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { createExpense } from '../controllers/expenseController';
+import { createExpense, getUserExpenses } from '../controllers/expenseController';
 
 const router = Router();
 
 router.post('/', createExpense);
+router.get('/user/:id', getUserExpenses);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRoutes from './userRoutes';
 import expenseRoutes from './expenseRoutes';
+import { getUserSummary } from '../controllers/summaryController';
 
 const router = Router();
 
 router.use('/users', userRoutes);
 router.use('/expenses', expenseRoutes);
+router.get('/summary/:id', getUserSummary);
 
 export default router;
